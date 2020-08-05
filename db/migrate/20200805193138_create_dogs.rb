87b30f42a00e4462207ec8fc3a75c2b56f1c17c6
Billy Ferrara <willfira717@gmail.com>
2020-08-05 19:36:06 +0000

    Automatically backed up by Learn

--- db/migrate/20200805193138_create_dogs.rb
+++ db/migrate/20200805193138_create_dogs.rb
@@ -1,9 +1,9 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def change 
+  def change
     create_table :dogs do |t|
-      t.string :name 
-      t.string :breed 
+      t.string :name
+      t.string :breed
     end 
-  end 
-  
+  end
+
 end
